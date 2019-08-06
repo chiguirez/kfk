1854a4f0a3d3b597f585e56f74a6b7f0add5997f
jose.ortiz <jose.ortiz@ubeeqo.com>
2019-08-06 08:52:51 +0200

    adding consumer. several handlers per message are allowed

--- kafka.go
+++ kafka.go
@@ -1,11 +1,153 @@
 package kfk
 
 import (
+	"context"
 	"encoding/json"
+	"os"
+	"os/signal"
 	"reflect"
+	"syscall"
+
+	"golang.org/x/sync/errgroup"
 
 	"github.com/Shopify/sarama"
 )
+
+// Consumer
+
+type KafkaDecodedMessage struct {
+	Body        interface{} `json:"message_body"`
+	MessageType string      `json:"message_type"`
+}
+
+//go:generate moq -out message_handler_mock.go . messageHandler
+type messageHandler interface {
+	Handle(KafkaDecodedMessage) error
+}
+
+type MessageHandlerList map[string][]messageHandler
+
+func NewMessageHandlerList() MessageHandlerList {
+	return MessageHandlerList{}
+}
+
+func (l MessageHandlerList) AddHandler(messageType string, handler messageHandler) {
+	l[messageType] = append(l[messageType], handler)
+}
+
+func (l MessageHandlerList) Handle(decodedMessage KafkaDecodedMessage) error {
+	handlers := l[decodedMessage.MessageType]
+	if len(handlers) == 0 {
+		return nil
+	}
+
+	errGroup := errgroup.Group{}
+
+	for _, handler := range handlers {
+		errGroup.Go(func() error {
+			return handler.Handle(decodedMessage)
+		})
+	}
+
+	return errGroup.Wait()
+}
+
+type KafkaConsumer struct {
+	consumerGroup sarama.ConsumerGroup
+	consumer      consumer
+}
+
+func NewKafkaConsumer(
+	kafkaBrokers []string,
+	consumerGroupID string,
+	handlerList MessageHandlerList,
+	topics []string,
+) (*KafkaConsumer, error) {
+	kafkaCfg := sarama.NewConfig()
+	kafkaCfg.Consumer.Return.Errors = true
+	kafkaCfg.Version = sarama.V1_0_0_0
+	kafkaCfg.Consumer.Offsets.Initial = sarama.OffsetOldest
+
+	consumerGroup, err := sarama.NewConsumerGroup(kafkaBrokers, consumerGroupID, kafkaCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	consumer := consumer{
+		ready:       make(chan bool, 0),
+		handlerList: handlerList,
+		topics:      topics,
+	}
+
+	return &KafkaConsumer{
+		consumer:      consumer,
+		consumerGroup: consumerGroup,
+	}, nil
+}
+
+func (c *KafkaConsumer) Start(ctx context.Context) error {
+	ctx, ctxCancel := context.WithCancel(ctx)
+
+	g, ctx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		for {
+			if err := c.consumerGroup.Consume(ctx, c.consumer.topics, &c.consumer); err != nil {
+				return err
+			}
+			if ctx.Err() != nil {
+				return ctx.Err()
+			}
+			c.consumer.ready = make(chan bool, 0)
+		}
+	})
+
+	<-c.consumer.ready
+
+	sigterm := make(chan os.Signal, 1)
+	signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
+	select {
+	case <-ctx.Done():
+	case <-sigterm:
+	}
+
+	ctxCancel()
+	if err := g.Wait(); err != nil {
+		return err
+	}
+
+	return c.consumerGroup.Close()
+}
+
+type consumer struct {
+	ready       chan bool
+	handlerList MessageHandlerList
+	topics      []string
+}
+
+func (c *consumer) Setup(sarama.ConsumerGroupSession) error {
+	close(c.ready)
+	return nil
+}
+
+func (c *consumer) Cleanup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (c *consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for message := range claim.Messages() {
+		var decodedMessage KafkaDecodedMessage
+		if err := json.Unmarshal(message.Value, &decodedMessage); err != nil {
+			return err
+		}
+
+		_ = c.handlerList.Handle(decodedMessage)
+		session.MarkMessage(message, "")
+	}
+
+	return nil
+}
+
+// Producer
 
 type KafkaProducer struct {
 	// TODO add async producer
