3a516dba009c211754e1c5d74bcf8bba3975005d
K4L1Ma <k4lima@gmail.com>
2019-10-26 11:10:42 +0200

    Added Fallback Handler for messages that can't be decoded

--- kafka_test.go
+++ kafka_test.go
@@ -2,6 +2,7 @@ package kfk
 
 import (
 	"context"
+	"encoding/json"
 	"os"
 	"testing"
 	"time"
@@ -32,6 +33,8 @@ func TestKafkaProduceAndConsume(t *testing.T) {
 		kafkaBroker = "localhost:9092"
 	}
 
+	ctx, cancel := context.WithCancel(context.Background())
+
 	setup := func() {
 
 		messageHandler := NewHandler(func(ctx context.Context, s sentTestingMessage) error {
@@ -53,6 +56,7 @@ func TestKafkaProduceAndConsume(t *testing.T) {
 	}
 
 	tearDown := func() {
+		cancel()
 		newKazoo, _ := kazoo.NewKazoo([]string{"localhost:2181"}, nil)
 		_ = newKazoo.DeleteTopicSync(topic, time.Second*5)
 		_ = newKazoo.Consumergroup(groupId).Delete()
@@ -71,11 +75,88 @@ func TestKafkaProduceAndConsume(t *testing.T) {
 
 		t.Run("When the consumer is started", func(t *testing.T) {
 			go func() {
-				err = kafkaConsumer.Start(context.Background())
+				err = kafkaConsumer.Start(ctx)
 				assert.NoError(err)
 			}()
 
 			t.Run("Then the message is consumed and the message information can be retrieved", func(t *testing.T) {
+				message := <-stChan
+
+				assert.Equal("testing-message-id", message.ID)
+				assert.Equal("testing-message-name", message.Name)
+			})
+		})
+	})
+}
+
+func TestKafkaFallbackConsume(t *testing.T) {
+	assert := require.New(t)
+
+	var (
+		kafkaConsumer *KafkaConsumer
+		kafkaProducer *KafkaProducer
+		err           error
+	)
+	stChan := make(chan sentTestingMessage)
+	topic := "topic-name-with-fallback"
+	groupId := "group-id"
+
+	kafkaBroker := os.Getenv("KAFKA_BROKER")
+	if kafkaBroker == "" {
+		kafkaBroker = "localhost:9092"
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	setup := func() {
+
+		messageHandler := func(ctx context.Context, s []byte) error {
+			message := &sentTestingMessage{}
+			if err := json.Unmarshal(s, message); err != nil {
+				return err
+			}
+			stChan <- *message
+			return nil
+		}
+
+		kafkaConsumer, err = NewKafkaConsumer(
+			[]string{kafkaBroker},
+			groupId,
+			[]string{topic},
+		)
+		assert.NoError(err)
+
+		kafkaConsumer.AddFallback(messageHandler)
+
+		kafkaProducer, err = NewKafkaProducer([]string{kafkaBroker})
+		assert.NoError(err)
+	}
+
+	tearDown := func() {
+		newKazoo, _ := kazoo.NewKazoo([]string{"localhost:2181"}, nil)
+		_ = newKazoo.DeleteTopicSync(topic, time.Second*5)
+		_ = newKazoo.Consumergroup(groupId).Delete()
+		cancel()
+	}
+
+	t.Run("Given a message is sent to kafka topic", func(t *testing.T) {
+		setup()
+		defer tearDown()
+
+		msg := sentTestingMessage{
+			ID:   "testing-message-id",
+			Name: "testing-message-name",
+		}
+		err = kafkaProducer.Send(topic, msg.ID, msg)
+		assert.NoError(err)
+
+		t.Run("When the consumer is started", func(t *testing.T) {
+			go func() {
+				err = kafkaConsumer.Start(ctx)
+				assert.NoError(err)
+			}()
+
+			t.Run("Then the message is consumed by the fallback and the message information can be retrieved", func(t *testing.T) {
 				message := <-stChan
 
 				assert.Equal("testing-message-id", message.ID)
